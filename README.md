d467945394dbec6b1dc3fdff5dd9564d9ad4035d
by <forestby@163.com>
2017-09-18 16:15:35 +0800

    start

---
+++ README.md
@@ -0,0 +1,20 @@
+# starvue
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+获取令牌：用于对未加密的token加密为与在app中的token一致
---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+<div id="app">
+	<img src="./assets/ppmiaologo.jpg">
+	<h1>{{ msg }}</h1>
+	<h2>小工具集合地</h2>
+	<ul>
+		<li><a href="#" target="_blank">获取令牌</a></li>
+		<li><a href="#" target="_blank">我是路人甲</a></li>
+		<li><a href="#" target="_blank">我是路人乙</a></li>
+		<li><a href="#" target="_blank">我是路人丙</a></li>
+	</ul>
+	<h2>vue集合点</h2>
+	<ul>
+		<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+		<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+		<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+		<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+		<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+		<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+		<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+		<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+	</ul>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	data() {
+		return {
+			msg: '拓展部落'
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
